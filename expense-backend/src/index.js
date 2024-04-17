66a1b8eed1778886c7d60490757535321f79d64f
Tchikaya Ariel <tchikayaline@gmail.com>
2024-04-17 17:35:26 +0100

    <feat>backend: setup winston in the backend

--- expense-backend/src/index.js
+++ expense-backend/src/index.js
@@ -1,19 +1,44 @@
 "use strict";
-// Importing 'express' module and 'dotenv' module
-const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config({ path: '.env' });
-const port = process.env.PORT || 3000;
-const app = express();
-// Define a route handler for the root endpoint '/'
-app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const winston_1 = require("winston");
+const express_winston_1 = __importDefault(require("express-winston"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const logger = (0, winston_1.createLogger)({
+    transports: [
+        new winston_1.transports.Console(),
+        new winston_1.transports.File({ filename: 'combined.log' })
+    ],
+    format: winston_1.format.combine(winston_1.format.timestamp(), winston_1.format.json()),
+    level: 'info' // Set log level
 });
-// Define a route handler for a new endpoint '/info'
+const port = process.env.PORT || 3000;
+const app = (0, express_1.default)();
+app.use(express_winston_1.default.logger({
+    transports: [
+        new winston_1.transports.Console(),
+        new winston_1.transports.File({ filename: 'http.log' })
+    ],
+    format: winston_1.format.combine(winston_1.format.timestamp(), winston_1.format.json()),
+    meta: false,
+    msg: 'HTTP {{req.method}} {{req.url}}',
+    expressFormat: true,
+    colorize: false,
+    ignoreRoute: function (req, res) { return false; }
+}));
+app.get('/', (req, res) => {
+    res.send('Express stop . + TypeScript Server');
+});
 app.get('/info', (req, res) => {
     res.send('This is the info endpoint');
 });
-// Start the Express server and listen on the defined port
+app.get('/health', (req, res) => {
+    res.json({ "ok": true });
+});
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
--- expense-backend/src/index.ts
+++ expense-backend/src/index.ts
@@ -1,29 +1,64 @@
-// Importing 'express' module and 'dotenv' module
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv"
+import { createLogger, transports, format } from 'winston';
+import expressWinston from 'express-winston';
+import dotenv from 'dotenv';
+import DailyRotateFile = require("winston-daily-rotate-file");
 
 dotenv.config();
+
+const logger = createLogger({
+    transports: [
+        new transports.Console(),
+        new DailyRotateFile({
+            filename: 'logs/%DATE%-combined.log',
+            datePattern: 'YYYY-MM-DD',
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '14d'
+        })
+    ],
+    format: format.combine(
+        format.timestamp(),
+        format.json()
+    ),
+    level: 'info' // Set log level
+});
 
 const port = process.env.PORT || 3000;
 
 const app: Express = express();
 
-// Define a route handler for the root endpoint '/'
+app.use(expressWinston.logger({
+    transports: [
+        new transports.Console(),
+        new transports.File({ filename: 'logs/http.log' })
+    ],
+    format: format.combine(
+        format.timestamp(),
+        format.json()
+    ),
+    meta: false,
+    msg: 'HTTP {{req.method}} {{req.url}}',
+    expressFormat: true,
+    colorize: false,
+    ignoreRoute: function (req, res) { return false; }
+}));
+
 app.get('/', (req: Request, res: Response ) => {
-    res.send('Express stop + TypeScript Server');
+    logger.info('Accessed the root endpoint');
+    res.send('Express stop . + TypeScript Server');
 });
 
-// Define a route handler for a new endpoint '/info'
 app.get('/info', (req: Request, res: Response ) => {
+    logger.info('Accessed the info endpoint');
     res.send('This is the info endpoint');
 });
 
-// endpoint respond for json 
 app.get('/health', (req: Request, res: Response ) => {
+    logger.info('Accessed the health endpoint');
     res.json({"ok": true});
 });
 
-// Start the Express server and listen on the defined port
-app.listen(port, () => { 
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(port, () => {
+    logger.info(`Server is running at http://localhost:${port}`);
 });
