cf6c5970e47739952b2958257de45e25035b04ca
bryandino673 <bdino673@gmail.com>
2024-04-19 11:38:02 +0100

    Added Tanstack-usequery Api

--- expense-frontend/src/App.tsx
+++ expense-frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import MyComponent from './components/MyComponent';
 function App() {
   return (
     <div className="App">
