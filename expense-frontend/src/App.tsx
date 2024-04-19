df89e2084f26ea96604e7ed2fce57f10d6340b3b
ValantineSuh <fuhvalentinesuh@gmail.com>
2024-04-19 09:08:27 +0100

    <feat>frontend: backend status component

--- expense-frontend/src/App.tsx
+++ expense-frontend/src/App.tsx
@@ -1,8 +1,37 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import React from 'react';
+import BackendHealthStatus from './components/BackendHealthStatus';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +39,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <BackendHealthStatus /> {/* Display the BackendHealthStatus component */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +51,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
