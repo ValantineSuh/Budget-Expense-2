1a578d5d2b6859513b7c8868d5c83a06f36651d1
Valsuh B <148695389+ValantineSuh@users.noreply.github.com>
2024-04-19 10:25:03 +0100

    Update App.tsx

--- expense-frontend/src/App.tsx
+++ expense-frontend/src/App.tsx
@@ -1,5 +1,3 @@
-// import React from 'react';
-// import logo from './logo.svg';
 import React from 'react';
 import BackendHealthStatus from './components/BackendHealthStatus';
 import logo from './logo.svg';
