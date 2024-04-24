fe32111acd465bbd1e4bfff7ac4457e1ef5448ca
Tchikaya Ariel <tchikayaline@gmail.com>
2024-04-24 16:57:47 +0100

    <feat>backend : build the ci

--- expense-backend/src/index.ts
+++ expense-backend/src/index.ts
@@ -13,37 +13,4 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-// Importing 'express','dotenv' modules and 'helmet'
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv"
-import helmet from 'helmet'
-
-dotenv.config();
-
-const port = process.env.PORT || 3000;
-
-const app: Express = express();
-
-// use helmet
-app.use(helmet());
-
-
-// Define a route handler for the root endpoint '/'
-app.get('/', (req: Request, res: Response ) => {
-    res.send('Express stop + TypeScript Server');
-});
-
-// Define a route handler for a new endpoint '/info'
-app.get('/info', (req: Request, res: Response ) => {
-    res.send('This is the info endpoint');
-});
-
-// endpoint respond for json 
-app.get('/health', (req: Request, res: Response ) => {
-    res.json({"ok": true});
-});
-
-// Start the Express server and listen on the defined port
-app.listen(port, () => { 
-    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
