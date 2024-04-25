ae670ade491c983f4113d4697430513201d5c0fc
Tchikaya Ariel <tchikayaline@gmail.com>
2024-04-25 12:07:32 +0100

    fixed some issues

--- expense-backend/src/index.ts
+++ expense-backend/src/index.ts
@@ -1,20 +1,3 @@
-// Importing necessary modules
-import express from 'express';
-
-// Creating an instance of Express
-const app = express();
-
-// Define a route
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-// Starting the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 import express, { Express, Request, Response } from "express";
 import { createLogger, transports, format } from 'winston';
 import expressWinston from 'express-winston';
